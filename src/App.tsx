e2b6c87f8192f7d7743e55b260f80962a44059f6
Wylliam Bassos <wylliam.bassos@fibraplac.com.br>
2022-03-17 10:55:12 -0300

    Ajuste Inicial do formulário

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { InputHTMLAttributes, useState } from 'react';
 import './App.css';
 
+interface Field extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+}
+
+interface FieldProps {
+  field: Field;
+}
+
+interface FormProps {
+  fields: Field[];
+}
+
 function App() {
+  const [name, setName] = useState('')
+  const [sobreNome, setSobreNome] = useState('')
+  const [idade, setIdade] = useState(0)
+
+  const fields: Field[] = [
+    { label: 'Nome', value: name, onChange: ({ target: { value } }) => setName(value) },
+    { label: 'Sobre Nome', value: sobreNome, onChange: ({ target: { value } }) => setSobreNome(value) },
+    { label: 'Idade', value: idade, type: 'number', onChange: ({ target: { value } }) => setIdade(Number(value)) }
+  ]
+
+  console.log(name)
+  console.log(fields)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        backgroundColor: '#282c34',
+        display: 'flex',
+        height: '100vh',
+        width: '100vw',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 'calc(10px + 2vmin)',
+        color: 'white',
+      }}
+    >
+      <div style={{
+        padding: '16px 32px 32px 32px',
+        backgroundColor: 'gray',
+        display: 'flex',
+        width: '25%',
+      }}>
+        <Form fields={fields} />
+      </div>
     </div>
   );
+}
+
+function Form({ fields }: FormProps) {
+  return (
+    <form style={{
+      width: '100%',
+    }}>
+      {fields.map(field => (
+        <Field key={field.label} field={field} />
+      ))}
+    </form>
+  )
+}
+
+function Field({ field }: FieldProps) {
+  const { label, ...rest } = field
+  return (
+    <div style={{
+      display: 'flex',
+      width: '100%',
+      justifyContent: 'space-between',
+      marginTop: 16,
+    }}>
+      <label>{label}</label>
+      <input
+        {...rest}
+        style={{
+          marginLeft: 16
+        }}
+      />
+    </div>
+  )
 }
 
 export default App;
